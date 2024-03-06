23115cdc5d8eeb9299fd4215d59556c5d35af2ed
gab <corbanigabriele@gmail.com>
2024-03-06 15:15:54 +0100

    ADD: Esercizio completo

--- app.js
+++ app.js
@@ -5,15 +5,38 @@
 
 let n = 100
 
-for(let i = 0; i < n; i++) {
+//html
+
+const containerElement = document.querySelector(".container")
+
+
+for(let i = 1 ; i <= n; i++) {
     
-    if(i % 3 === 0) {
-        console.log("Fizz")
-    } else if(i % 5 === 0) {
-        console.log("Buzz")
-    }
-    else{
-        console.log("FizzBuzz")
+    if(i % 3 === 0 && i % 5 === 0) {
+        const pElement = document.createElement("p")
+        pElement.style.backgroundColor = "red"
+        pElement.textContent = "FizzBuzz"
+        containerElement.append(pElement)
+
+    } else if(i % 3 === 0) {
+        const pElement = document.createElement("p")
+        pElement.style.backgroundColor = "green"
+        pElement.textContent = "Fizz"
+        containerElement.append(pElement)
     }
 
+    else if(i % 5 === 0) {
+        const pElement = document.createElement("p")
+        pElement.style.backgroundColor = "yellow"
+        pElement.textContent = "Buzz"
+        containerElement.append(pElement)
+
+    } else {
+        const pElement = document.createElement("p")
+        pElement.style.backgroundColor = "aqua"
+        pElement.textContent = i
+        containerElement.append(pElement)
+        
+    }
 }
+
